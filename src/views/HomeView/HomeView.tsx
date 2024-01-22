798c7284579c68c2d8c455c5388f1154e9b577e2
MarieUc <marie.ucles@covage.com>
2024-01-22 19:15:07 +0100

    vidéos 6 ok

--- src/views/HomeView/HomeView.tsx
+++ src/views/HomeView/HomeView.tsx
@@ -18,9 +18,10 @@ import {
 
 import { Pokemon } from '../../models/Pokemon';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { listPokeOriginal } from '../../data/Pokemon.List';
+import { getRandomInt, shuffle } from '../../utils/utils';
 import * as commonStyle from '../../utils/commonStyle';
-function HomeView(): JSX.Element {
+
+const HomeView = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -28,7 +29,8 @@ function HomeView(): JSX.Element {
   };
 
   const [counterPokedex, setCounterPokedex] = useState(0);
-  const [listPoke, setListPoke] = useState([...listPokeOriginal]);
+  const [listPoke, setListPoke] = useState<Pokemon[]>(undefined);
+  const [isDataReceived, setIsDataReceived] = useState(false);
 
   const getNamePokemon = (namePokemon: string) => {
     console.log('View details for', namePokemon);
@@ -36,67 +38,102 @@ function HomeView(): JSX.Element {
   };
 
   const modifyLevel = () => {
-    let newArr = [...listPoke]; // copying the old datas array
-    newArr[counterPokedex].level = listPoke[counterPokedex].level + 5;
-    setListPoke(newArr);
-  }
+    if (listPoke) {
+      let newArr = [...listPoke];
+      newArr[counterPokedex].level = listPoke[counterPokedex].level + 5;
+      setListPoke(newArr);
+    }
+  };
+
   const onNext = () => {
-    if (counterPokedex === listPoke.length - 1) {
+    if (listPoke && counterPokedex === listPoke.length - 1) {
       setCounterPokedex(0);
-    } else {
+    } else if (listPoke) {
       setCounterPokedex(counterPokedex + 1);
     }
   };
 
   const onPrevious = () => {
-    if (counterPokedex === 0) {
+    if (listPoke && counterPokedex === 0) {
       setCounterPokedex(listPoke.length - 1);
-    } else {
+    } else if (listPoke) {
       setCounterPokedex(counterPokedex - 1);
     }
-  }
+  };
+
+  const fetchPokemon = () => {
+    const url = 'https://pokeapi.co/api/v2/pokemon?limit=151';
+    fetch(url)
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+  
+        const newArray = json.results.map((pokemonData: any, index: number) => {
+          let indexPokedex = index + 1;
+          let pokemon = {
+            id: indexPokedex,
+            level: getRandomInt(40, 80),
+            isMale: true,
+            src: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${indexPokedex}.png`,
+            ...pokemonData,
+          };
+  
+          console.log('URL de l\'image pour', pokemon.name, ':', pokemon.src);
+  
+          return pokemon;
+        });
+  
+        setListPoke(shuffle(newArray));
+        setIsDataReceived(true);
+      })
+      .catch(error => {
+        console.log('Error: ', error);
+      });
+  };
+  
+  
+
+  useEffect(() => {
+      fetchPokemon();
+  }, [])
+
   return (
-    <View  style = {styles.main_container}>
-      <View style = {styles.title_container} >
-     <Text style={styles.text_title}>Pokédex Application</Text>
-     </View>
-     <View style = {styles.pokemon_container}>
-
-     
-      <PokemonInfo
-        id={listPoke[counterPokedex]?.id}
-        name={listPoke[counterPokedex]?.name}
-        level={listPoke[counterPokedex]?.level}
-        isMale={listPoke[counterPokedex]?.isMale}
-        src={listPoke[counterPokedex]?.src}
-        OnClickPokemon={modifyLevel}
-      />
+    <View style={styles.main_container}>
+      <View style={styles.title_container}>
+        <Text style={styles.text_title}>Pokédex Application</Text>
       </View>
-       <View style = {styles.button_container}>
-
-       <TouchableOpacity
-            style={styles.buttonNextPrevious}
-            onPress={() => onPrevious()}
-            >
-              <Image source={require('../../assets/images/left-arrow.png')} style={styles.iconButton} />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.buttonNextPrevious}
-            onPress={() => onNext()}
-            >
-              <Image source={require('../../assets/images/right-arrow.png')} style={styles.iconButton} />
-          </TouchableOpacity>
+      <View style={styles.pokemon_container}>
+        {isDataReceived ? (
+          <PokemonInfo
+            id={listPoke[counterPokedex].id}
+            name={listPoke[counterPokedex].name}
+            level={listPoke[counterPokedex].level}
+            isMale={listPoke[counterPokedex].isMale}
+            src={listPoke[counterPokedex].src}
+            onClickPokemon={modifyLevel}
+          />
+        ) : (
+          <Text>This is loading</Text>
+        )}
+      </View>
+      <View style={styles.button_container}>
+        <TouchableOpacity style={styles.buttonNextPrevious} onPress={() => onPrevious()}>
+          <Image source={require('../../assets/images/left-arrow.png')} style={styles.iconButton} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonNextPrevious} onPress={() => onNext()}>
+          <Image source={require('../../assets/images/right-arrow.png')} style={styles.iconButton} />
+        </TouchableOpacity>
       </View>
     </View>
   );
 };
 
-const PokemonInfo = ({ name, level, isMale, src, OnClickPokemon }: Pokemon) => {
+const PokemonInfo = ({ name, level, isMale, src, onClickPokemon }: Pokemon) => {
   return (
     <>
-      <Text style={styles.text_appeared}> A new Pokemon appared !</Text>
-      <TouchableOpacity onPress={() => OnClickPokemon?.()}>
-        <Image source={src} style={styles.imagePokemon} />
+      <Text style={styles.text_appeared}> A new Pokemon appeared !</Text>
+      <TouchableOpacity onPress={() => onClickPokemon?.()}>
+        <Image source={{ uri: src }} style={styles.imagePokemon} />
       </TouchableOpacity>
       <Text>His name is {name}, his level is {level}.</Text>
       {isMale ? <Text>This is a male</Text> : <Text>This is a female</Text>}
@@ -104,34 +141,33 @@ const PokemonInfo = ({ name, level, isMale, src, OnClickPokemon }: Pokemon) => {
   );
 };
 
-
 const styles = StyleSheet.create({
   main_container: {
-      flex: 1
+    flex: 1,
   },
 
   title_container: {
     flex: 1,
-    alignItems: 'center'
+    alignItems: 'center',
   },
 
   pokemon_container: {
-      flex: 4,
-      justifyContent: 'center',
-      alignItems: 'center'
+    flex: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 
   button_container: {
-      flex: 1,
-      flexDirection: 'row',
-      justifyContent: 'space-around'
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
 
   text_title: {
     fontSize: 26,
     fontWeight: 'bold',
     color: 'rgb(200, 0, 0)',
-    marginTop:  30
+    marginTop: 30,
   },
   imagePokemon: {
     width: 200,
@@ -140,20 +176,19 @@ const styles = StyleSheet.create({
 
   iconButton: {
     width: 40,
-    height: 40
+    height: 40,
   },
 
   text_appeared: {
     marginBottom: 20,
     fontStyle: 'italic',
-    fontSize: 18
+    fontSize: 18,
   },
 
- // @ts-ignore
- buttonNextPrevious: {
-  ...commonStyle.elevationButton,
-  ...commonStyle.roundedButton
-}
+  buttonNextPrevious: {
+    ...commonStyle.elevationButton,
+    ...commonStyle.roundedButton,
+  },
 });
 
 export default HomeView;
