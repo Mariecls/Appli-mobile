91f59ed4f407c9cc3b75d780e9285f8769448d79
MarieUc <marie.ucles@covage.com>
2024-01-23 15:45:46 +0100

    App ok

--- src/views/PokemonDetailsView/PokemonDetailsView.tsx
+++ src/views/PokemonDetailsView/PokemonDetailsView.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const PokemonDetailsView = ({ route }) => {
   const { id, name, src } = route.params;
